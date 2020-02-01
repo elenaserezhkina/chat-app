29cf7ca53b41617337fad100a2dadbb394b05349
Olena Shutovska <anel92r@gmail.com>
2020-02-01 21:10:20 +0100

    separate file with array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import React from "react";
 import "./App.css";
 import Contact from "./components/Contact.js";
-
-const users = [
-  {
-    name: "Brand Gibson",
-    picture: "https://randomuser.me/api/portraits/thumb/men/75.jpg",
-    status: true
-  },
-  {
-    name: "Laurie Campbell",
-    picture: "https://randomuser.me/api/portraits/thumb/women/18.jpg",
-    status: false
-  },
-  {
-    name: "Yasemin Duygulu",
-    picture: "https://randomuser.me/api/portraits/thumb/women/70.jpg",
-    status: true
-  }
-];
+import users from "./components/ContactList";
 
 function App() {
   return (
---
+++ src/components/ContactList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const users = [
+  {
+    name: "Brand Gibson",
+    picture: "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+    status: true
+  },
+  {
+    name: "Laurie Campbell",
+    picture: "https://randomuser.me/api/portraits/thumb/women/18.jpg",
+    status: false
+  },
+  {
+    name: "Yasemin Duygulu",
+    picture: "https://randomuser.me/api/portraits/thumb/women/70.jpg",
+    status: true
+  },
+  {
+    name: "Yannic Moecke",
+    picture:
+      "https://cdn.shopify.com/s/files/1/0066/8719/5183/products/KylieSkinMasks3.jpg?v=1572045947",
+    status: false
+  },
+  {
+    name: "Melina Torsteerson",
+    picture:
+      "https://i2.wp.com/ocean-now.org/wp-content/uploads/2019/06/WEB-72dpi-sRGB-OceanNow_InYourFace_MartinZadak-054-PhotoBySaskiaUppenkamp-PhotoBySaskiaUppenkamp.jpg?fit=667%2C1000&ssl=1",
+    status: true
+  }
+];
+
+export default users;
